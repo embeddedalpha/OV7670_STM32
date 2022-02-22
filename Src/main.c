57a174de69d03ec74eb081393eca03a489c32004
Kunal Salvi <kunalsalvi63@gmail.com>
2022-02-22 12:36:41 +0530

    Updated main,c

--- Src/main.c
+++ Src/main.c
@@ -7,9 +7,17 @@
 //  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 //#endif
 
+
+void OV7670_Parallel_Port_Init(GPIO_TypeDef *port);
+uint8_t OV7670_Parallel_Read(GPIO_TypeDef *port);
+void OV7670_Parallel_Write(GPIO_TypeDef *port, uint8_t data);
+
+
+
+
+
 int main(void)
 {
-
 
 
 	for(;;);
